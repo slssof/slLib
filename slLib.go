7cfeb8fa108ce493adafe0717b911accd0129f19
slssof <sl_sof@mail.ru>
2024-10-31 00:13:04 +0800

    Add files via upload

--- slLib.go
+++ slLib.go
@@ -9,7 +9,7 @@ type RNG struct {
 	state uint32
 }
 
-func (rng *RNG) init(seed uint32) {
+func (rng *RNG) Init(seed uint32) {
 	rng.m = 0x100000000 - 1 // 2**32;
 	rng.a = 1103515245
 	rng.c = 12345
@@ -21,17 +21,17 @@ func (rng *RNG) init(seed uint32) {
 	}
 }
 
-func (rng *RNG) nextInt() uint32 {
+func (rng *RNG) NextInt() uint32 {
 	rng.state = (rng.a*rng.state + rng.c) % rng.m
 	return rng.state
 }
 
-func (rng *RNG) nextFloat() float64 {
+func (rng *RNG) NextFloat() float64 {
 	// returns in range [0,1]
 	return float64(rng.nextInt() / (rng.m - 1))
 }
 
-func (rng *RNG) nextRange(start uint32, end uint32) uint32 {
+func (rng *RNG) NextRange(start uint32, end uint32) uint32 {
 	// returns in range [start, end): including start, excluding end
 	// can't modulu nextInt because of weak randomness in lower bits
 	var rangeSize uint32
@@ -41,7 +41,7 @@ func (rng *RNG) nextRange(start uint32, end uint32) uint32 {
 	return start + uint32(randomUnder1*float32(rangeSize))
 }
 
-func (rng *RNG) dice(number int, dice int, plus int) int {
+func (rng *RNG) Dice(number int, dice int, plus int) int {
 	var s int
 	s = 0
 	for i := 0; i < number; i++ {
