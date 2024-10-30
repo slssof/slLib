da4e0ce51ac83df0e5fab9cd26ba299e8ad5b8ee
slssof <sl_sof@mail.ru>
2024-10-30 23:36:08 +0800

    Add files via upload
    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module slLib
+
+go 1.22.2
---
+++ slLib.go
@@ -0,0 +1,51 @@
+// RNG project RNG.go
+package slLib
+
+type RNG struct {
+	// LCG using GCC's constants
+	m     uint32
+	a     uint32
+	c     uint32
+	state uint32
+}
+
+func (rng *RNG) init(seed uint32) {
+	rng.m = 0x100000000 - 1 // 2**32;
+	rng.a = 1103515245
+	rng.c = 12345
+	if seed == 0 {
+		rng.state = 234236743 * (rng.m - 1)
+	}
+	if seed != 0 {
+		rng.state = seed
+	}
+}
+
+func (rng *RNG) nextInt() uint32 {
+	rng.state = (rng.a*rng.state + rng.c) % rng.m
+	return rng.state
+}
+
+func (rng *RNG) nextFloat() float64 {
+	// returns in range [0,1]
+	return float64(rng.nextInt() / (rng.m - 1))
+}
+
+func (rng *RNG) nextRange(start uint32, end uint32) uint32 {
+	// returns in range [start, end): including start, excluding end
+	// can't modulu nextInt because of weak randomness in lower bits
+	var rangeSize uint32
+	rangeSize = end - start
+	var randomUnder1 float32
+	randomUnder1 = float32(rng.nextInt()) / float32(rng.m)
+	return start + uint32(randomUnder1*float32(rangeSize))
+}
+
+func (rng *RNG) dice(number int, dice int, plus int) int {
+	var s int
+	s = 0
+	for i := 0; i < number; i++ {
+		s = s + int(rng.nextRange(1, uint32(dice+1)))
+	}
+	return s + plus
+}
